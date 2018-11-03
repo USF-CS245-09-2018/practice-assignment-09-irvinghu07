8e8d4f23bef2d78657d1e1c7c3ef4337b79e86e2
irvinghu07 <rhu5@dons.usfca.edu>
2018-11-02 22:53:31 -0700

    setting up

---
+++ src/BinaryHeap.java
@@ -0,0 +1,16 @@
+/**
+ * Development IDE: IntelliJ IDEA
+ * Author: irving
+ * Project Name: cs245-assignment-09
+ * Date: 2018-11-02
+ */
+public class BinaryHeap {
+
+    public void add(Integer val) {
+
+    }
+
+    public Integer remove() {
+        return null;
+    }
+}
